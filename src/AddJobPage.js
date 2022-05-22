2224ddd31ea288d95a78238b87301a2f9879d41c
Niques928 <chiniquejoseph@gmail.com>
2022-05-23 01:49:38 +0300

    added: AddJobPage

---
+++ src/AddJobPage.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { postJob } from "./functions";
+
+const AddJobPage = () => {
+  const [description, setDescription] = useState("");
+  const [employment, setEmployment] = useState("");
+  const [salary, setSalary] = useState(0);
+  const [title, setTitle] = useState("");
+
+  const onCreate = () => {
+    if (!description || !employment || !salary || title) return;
+    // postJob({ description, employment, salary, title });
+  };
+
+  return (
+    <form
+      onSubmit={() => onCreate()}
+      style={{ display: "flex", flexDirection: "column", margin: "3% 2%" }}
+    >
+      <div style={{ margin: "5px" }}>
+        <input
+          onChange={(event) => setTitle(event.target.value)}
+          value={title}
+          placeholder={"Title"}
+        />
+      </div>
+      <div style={{ margin: "5px" }}>
+        <textarea
+          placeholder={"Description"}
+          onChange={(event) => setDescription(event.target.value)}
+          value={description}
+        />
+      </div>
+      <div style={{ margin: "5px" }}>
+        <input
+          placeholder={"Employment"}
+          onChange={(event) => setEmployment(event.target.value)}
+          value={employment}
+        />
+      </div>
+      <div style={{ margin: "5px" }}>
+        <input
+          placeholder={"Salary"}
+          type={"number"}
+          onChange={(event) => setSalary(event.target.value)}
+          value={salary}
+        />
+      </div>
+      <div style={{ margin: "5px" }}>
+        <button type={"submit"}>Create</button>
+      </div>
+    </form>
+  );
+};
+
+export default AddJobPage;
