71fd10c74c4c117c06976904999dd5b464aa1c86
Niques928 <chiniquejoseph@gmail.com>
2022-05-23 02:37:51 +0300

    provided: loading in CompaniesPage

--- src/CompaniesPage.js
+++ src/CompaniesPage.js
@@ -3,11 +3,17 @@ import { getCandidates, getCompanies } from "./functions";
 
 const CompaniesPage = () => {
   const [companies, setCompanies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    getCompanies().then((data) => setCompanies(data));
+    setLoading(true);
+    getCompanies()
+      .then((data) => setCompanies(data))
+      .finally(() => setLoading(false));
   }, []);
   return (
     <div style={{ margin: "2% 5%" }}>
+      {loading && <h1>Loading...</h1>}
       {companies.map((elem, index) => (
         <div style={{ border: "1px solid black" }}>
           <h1 style={{ fontSize: "16px" }}>{elem.name}</h1>
