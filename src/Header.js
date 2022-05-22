43ca43745aabc105cca656c6605929dbf581d186
Niques928 <chiniquejoseph@gmail.com>
2022-05-23 02:25:16 +0300

    added:header brandName

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ firstName, lastName }) => {
+  let navigate = useNavigate();
+  const redirectHome = () => {
+    navigate("/");
+  };
+
   return (
     <header className={"header"}>
+      <span className={"brandName"} onClick={() => redirectHome()}>
+        Ad platform
+      </span>
       <div className={"fullname"}>
         {firstName && lastName ? (
           <>
